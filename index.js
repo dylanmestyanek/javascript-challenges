7aba8327ee1e6660aa08964dfadcf4c977713def
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-03-02 09:49:05 -0600

    count-fruits.js

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
 
 
 
-function countApplesAndOranges(s, t, a, b, apples, oranges) {
-    // s: integer, starting point of Sam's house location.
-    // t: integer, ending location of Sam's house location.
-    // a: integer, location of the Apple tree.
-    // b: integer, location of the Orange tree.
-    // apples: integer array, distances at which each apple falls from the tree.
-    // oranges: integer array, distances at which each orange falls from the tree.
-    
-    // Take the location of the apple tree, and orange tree and add their 'fall' distance to location         value
-    // Iterate over apples/oranges array and add the appropriate tree location value to each (a or b)
-    // Check each value in new array, and see if any values fall in the range of s <--> t
-    function countFruit(fruits) {
-        let count = 0
-        fruits.forEach(fruit => {
-            console.log("FRUIT", fruit, "S", s, "T", t) 
-            if (fruit >= s && fruit <= t) {
-                count++
-            }
-        })
-
-        return count
-    }
-
-    apples = apples.map(apple => apple + a)
-    oranges = oranges.map(orange => orange + b)
-    console.log(countFruit(apples))
-    console.log(countFruit(oranges))
-}
-
-countApplesAndOranges(7, 10, 4, 12, [2, 3, -4], [3, -2, -4])
