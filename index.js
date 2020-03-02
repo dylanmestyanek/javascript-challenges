85c3647c8196cb34ba92ab02f76272bd63733d92
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-03-02 09:47:20 -0600

    plus-minus.js

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-// function plusMinus(arr) {
-//     // have a variable for postive, negative, and zeros
-//     // retrieve count of how frequently these values appear in the array
-//     // divide these counts by the length of the array to 6 decimal places
-//     const positives = arr.filter(v => v > 0).length
-//     const negatives = arr.filter(v => v < 0).length
-//     const zeros = arr.filter(v => v === 0).length
 
-//     console.log(positives)
-//     console.log(negatives)
-//     console.log(zeros)
+function staircase(n) {
+    for (let i = 1; i <= n; i ++){
+        console.log(' '.repeat(n - i) + '#'.repeat(i))
+    }
 
-// }
+}
 
-// plusMinus([-4, 3, -9, 0, 4, 1])
-
-// function staircase(n) {
-//     for (let i = 1; i <= n; i ++){
-//         console.log(' '.repeat(n - i) + '#'.repeat(i))
-//     }
-
-// }
-
-// staircase(6)
+staircase(6)
 
 // function timeConversion(s) {
 //     let timeOfDay = s.slice(-2);
