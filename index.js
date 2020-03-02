ea744535221f6ca16c9612eafd19a2be22782e4d
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-03-02 09:48:05 -0600

    time-conversion.js

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 
 
-// function timeConversion(s) {
-//     let timeOfDay = s.slice(-2);
-//     let hours = +s.slice(0, 2);
-
-//     if (timeOfDay === 'PM') {
-//         hours < 12 && (hours += 12)
-//         return String(hours) + s.slice(2, -2)
-//     } else {
-//         if (hours === 12) {
-//             hours = '00';
-//             s = hours + s.slice(2);
-//         }
-//         return s.slice(0, -2)
-//     }
-
-// }
-
-// console.log(timeConversion("12:05:45PM"))
-
 // function gradingStudents(grades) {
 //     // if grade - next multiple of 5 < 3, round up to next 5 mult
 //     // else if grade < 40, return grade
