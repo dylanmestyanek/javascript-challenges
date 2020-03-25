f7bd202811a89f4d545d0ed29875633e2cc6885c
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-03-25 16:03:46 -0500

    Completed exercises 1 - 4 on closures.js

--- 3-16-2020/findDigits.js
+++ 3-16-2020/findDigits.js
@@ -1,6 +1,7 @@
 function findDigits(n) {
     var splitNumber = String(n).split('').map(function (v) { return +v; });
     return splitNumber.filter(function (num) { return n % num == 0; }).length;
-};
+}
+;
 console.log(findDigits(12));
 console.log(findDigits(1012));
