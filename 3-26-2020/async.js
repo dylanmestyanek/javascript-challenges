70f152cf6edca6b0ab3d6efa3846d73142ea04c3
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-03-27 11:45:31 -0500

    I am stuck on async 10 challenge :(

--- 3-26-2020/async.js
+++ 3-26-2020/async.js
@@ -95,45 +95,69 @@ console.log("\n---------------\n[CHALLENGE] 8:\n---------------")
   
   /* CHALLENGE 9 */
 console.log("\n---------------\n[CHALLENGE] 9:\n---------------")
-  class SecondClock {
-    constructor(cb) {
-      this.cb = cb;
-      this.seconds = 55;
-      this.timer = 0;
-    }
+  // class SecondClock {
+  //   constructor(cb) {
+  //     this.cb = cb;
+  //     this.seconds = 55;
+  //     this.timer = 0;
+  //   }
 
-    start() {
-      this.timer = setInterval(() => {
-        this.seconds > 60 && (this.seconds = 1);
-        this.cb(this.seconds++);
-      }, 1000);
-    }
+  //   start() {
+  //     this.timer = setInterval(() => {
+  //       this.seconds > 60 && (this.seconds = 1);
+  //       this.cb(this.seconds++);
+  //     }, 1000);
+  //   }
 
-    reset() {
-      clearInterval(this.timer);
-    }
-  }
+  //   reset() {
+  //     clearInterval(this.timer);
+  //   }
+  // }
   
   // UNCOMMENT THESE TO TEST YOUR WORK!
-  const clock = new SecondClock((val) => { console.log(val) });
-  console.log("Started Clock.");
-  clock.start();
-  setTimeout(() => {
-      clock.reset();
-      console.log("Stopped Clock after 6 seconds.");
-  }, 6000);
+  // const clock = new SecondClock((val) => { console.log(val) });
+  // console.log("Started Clock.");
+  // clock.start();
+  // setTimeout(() => {
+  //     clock.reset();
+  //     console.log("Stopped Clock after 6 seconds.");
+  // }, 6000);
   
   /* CHALLENGE 10 */
   
   function debounce(callback, interval) {
-    // ADD CODE HERE
+    let timer = 0;
+    let called = 0;
+    let seconds = 0;
+
+    const innerFunc = () => {
+      // timer = setInterval(() => seconds += 1000, 1000);
+
+      console.log("SECONDS", seconds)
+      if (called === 0) {
+        console.log("hit the 1 block")
+        called++;
+        return callback();
+      } else if (seconds === interval) {
+        console.log("hit the 2 block")
+        return callback();
+      } else {
+        console.log("hit the 3 block")
+        seconds = 0;
+        // clearInterval(timer);
+      }
+
+      return "done"
+    };
+
+    return innerFunc;
   }
   
   // UNCOMMENT THESE TO TEST YOUR WORK!
-  // function giveHi() { return 'hi'; }
-  // const giveHiSometimes = debounce(giveHi, 3000);
-  // console.log(giveHiSometimes()); // -> 'hi'
-  // setTimeout(function() { console.log(giveHiSometimes()); }, 2000); // -> undefined
+  function giveHi() { return 'hi'; }
+  const giveHiSometimes = debounce(giveHi, 3000);
+  console.log(giveHiSometimes()); // -> 'hi'
+  setTimeout(function() { console.log(giveHiSometimes()); }, 2000); // -> undefined
   // setTimeout(function() { console.log(giveHiSometimes()); }, 4000); // -> undefined
   // setTimeout(function() { console.log(giveHiSometimes()); }, 8000); // -> 'hi'
   
