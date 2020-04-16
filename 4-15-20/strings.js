4624053be056b91ce907423ea815a7c93690430c
dylanmestyanek <mestyanekdylan@gmail.com>
2020-04-16 17:16:10 -0500

    function works if only one letter appears twice. Need to take into consideration when multiple values appear next to each other

--- 4-15-20/strings.js
+++ 4-15-20/strings.js
@@ -1,25 +1,15 @@
 function superReducedString(s) {
-    let changed = true;
-    let i = 0;
 
-    while (changed) {
-        changed = false;
-        if (i = s.length - 1) {
-            i = 0;
+   for (let i = 0; i < s.length; i++){
+        if (s[i + 1]){
+            if (s[i] === s[i + 1]){
+                s = s.slice(i + 2)
+            }
+
         }
+   }
 
-        if (s[i] === s[i + 1]) {
-            s = s.slice(s + 2);
-            changed = true;
-            i++;
-        } 
-
-        if (!changed) {
-            return
-        }
-    }
-
-    return s
+   return s
 }
 
-console.log(superReducedString("aabc"))
+console.log(superReducedString("aabbc"))
